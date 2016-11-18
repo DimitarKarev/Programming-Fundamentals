0bf868d44fae0f7230747e70fd547c7e7d0ce534
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-18 23:44:31 +0200

    Solving Master Number

--- 03. Methods and Debugging - Exercises/12. Master Number/12. Master Number.cs
+++ 03. Methods and Debugging - Exercises/12. Master Number/12. Master Number.cs
@@ -10,6 +10,52 @@ namespace _12.Master_Number
     {
         static void Main(string[] args)
         {
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (SumOfDigitsDividesBy7(i) && IsPalindrome(i) && HoldsEvenDigit(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        private static bool IsPalindrome(int i)
+        {
+            bool isPalindrome = true;
+            string number = Convert.ToString(i);
+            string reversed = "";
+            for (int j = number.Length - 1; j >= 0; j--)
+            {
+                reversed += number[j];
+            }
+            if (number != reversed) isPalindrome = false;
+            return isPalindrome;
+        }
+
+        private static bool SumOfDigitsDividesBy7(int i)
+        {
+            var sum = 0;
+            bool IsDivisibleBy7 = false;
+            while (i > 0)
+            {
+                sum += i % 10;
+                i /= 10;
+            }
+            if (sum % 7 == 0) IsDivisibleBy7 = true;
+            return IsDivisibleBy7;
+        }
+            
+        private static bool HoldsEvenDigit(int i)
+        {
+            bool evenDigit = false;
+            while (i > 0)
+            {
+                var digit = i % 10;
+                if (digit % 2 == 0) evenDigit = true;
+                i /= 10;
+            }
+            return evenDigit;
         }
     }
 }
