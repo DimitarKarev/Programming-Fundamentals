3fd24e8e031b59e07a0c6e3e55a51c0e54098c7b
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-18 23:46:17 +0200

    Solving Factorial Trailing Zeroes

--- 03. Methods and Debugging - Exercises/14. Factorial Trailing Zeroes/14. Factorial Trailing Zeroes.cs
+++ 03. Methods and Debugging - Exercises/14. Factorial Trailing Zeroes/14. Factorial Trailing Zeroes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace _14.Factorial_Trailing_Zeroes
 {
@@ -10,6 +11,36 @@ namespace _14.Factorial_Trailing_Zeroes
     {
         static void Main(string[] args)
         {
+            var n = int.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+            var trailingZeroes = 0;
+
+            factorial = CalculateFactorial(n, factorial);
+            trailingZeroes = CalcTrailingZeroes(factorial, trailingZeroes);
+
+            Console.WriteLine(trailingZeroes);
+        }
+
+        private static int CalcTrailingZeroes(BigInteger factorial, int trailingZeroes)
+        {
+            BigInteger currentDigit;
+            while (true)
+            {
+                currentDigit = factorial % 10;
+                if (currentDigit == 0) trailingZeroes++;
+                else break;
+                factorial /= 10;
+            }
+            return trailingZeroes;
+        }
+
+        private static BigInteger CalculateFactorial(int n, BigInteger factorial)
+        {
+            for (int i = 2; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
         }
     }
 }
