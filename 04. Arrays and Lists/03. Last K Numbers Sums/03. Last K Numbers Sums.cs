204a3c4ab8fee8d8c83de0675a8df37fb534f35c
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-20 14:25:28 +0200

    Solving Last K Numbers Sums

--- 04. Arrays and Lists/03. Last K Numbers Sums/03. Last K Numbers Sums.cs
+++ 04. Arrays and Lists/03. Last K Numbers Sums/03. Last K Numbers Sums.cs
@@ -10,7 +10,36 @@ namespace _03.Last_K_Numbers_Sums
     {
         static void Main(string[] args)
         {
-            
+            var n = int.Parse(Console.ReadLine());
+            var k = int.Parse(Console.ReadLine());
+            var arr = new long[n];
+            arr[0] = 1;
+
+            GetingArrayValues(k, arr);
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        private static void GetingArrayValues(int k, long[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                long sum = 0;
+                var length = i - k;
+                if (length < 0) length = 0;
+                sum = SumLastKElement(arr, i, sum, length);
+                arr[i] = sum;
+            }
+        }
+
+        private static long SumLastKElement(long[] arr, int i, long sum, int length)
+        {
+            for (int j = length; j < i; j++)
+            {
+                sum += arr[j];
+            }
+
+            return sum;
         }
     }
 }
