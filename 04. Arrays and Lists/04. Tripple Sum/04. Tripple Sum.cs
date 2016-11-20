7e1e6d16099334bb8bd01b74a751fc77a5dcf903
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-20 15:05:03 +0200

    Solving Tripple Sum

--- 04. Arrays and Lists/04. Tripple Sum/04. Tripple Sum.cs
+++ 04. Arrays and Lists/04. Tripple Sum/04. Tripple Sum.cs
@@ -10,6 +10,38 @@ namespace _04.Tripple_Sum
     {
         static void Main(string[] args)
         {
+            var input = Console.ReadLine();
+            string[] stringArr = input.Split(' ');
+            var intArr = new long[stringArr.Length];
+            bool noSum = true;
+            ConvertingToIntegers(stringArr, intArr);
+            noSum = CheckingForSums(intArr, noSum);
+            if (noSum) Console.WriteLine("No");
         }
+
+        private static void ConvertingToIntegers(string[] stringArr, long[] intArr)
+        {
+            for (long i = 0; i < intArr.Length; i++)
+            {
+                intArr[i] = long.Parse(stringArr[i]);
+            }
+        }
+
+        private static bool CheckingForSums(long[] intArr, bool noSum)
+        {
+            for (int a = 0; a < intArr.Length; a++)
+                for (int b = a + 1; b < intArr.Length; b++)
+                {
+                    long sum = intArr[a] + intArr[b];
+                    if (intArr.Contains(sum))
+                    {
+                        Console.WriteLine($"{intArr[a]} + {intArr[b]} == {sum}");
+                        noSum = false;
+                    }
+                }
+            return noSum;
+        }
+
+       
     }
 }
