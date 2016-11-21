7e81c338293cb7d5a95c094318db410c0d637ef0
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-21 13:47:35 +0200

    Solving Sum Arrays

--- 04. Arrays and Lists/07. Sum Arrays/07. Sum Arrays.cs
+++ 04. Arrays and Lists/07. Sum Arrays/07. Sum Arrays.cs
@@ -10,7 +10,17 @@ namespace _07.Sum_Arrays
     {
         static void Main(string[] args)
         {
-           
+            var arr1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var arr2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var shorterArray = Math.Min(arr1.Length, arr2.Length);
+            var longerArray = Math.Max(arr1.Length, arr2.Length);
+            var arrSum = new int[longerArray];
+
+            for (int i = 0; i < arrSum.Length; i++)
+            {   
+                arrSum[i] = arr1[i % arr1.Length] + arr2[i % arr2.Length];
+            }
+            Console.WriteLine(string.Join(" ",arrSum));
         }
     }
 }
