d340278ac7943856def4f9f1623f0dc73448e0f5
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-21 13:24:10 +0200

    Solving Rounding Numbers

--- 04. Arrays and Lists/05. Rounding Numbers/05. Rounding Numbers.cs
+++ 04. Arrays and Lists/05. Rounding Numbers/05. Rounding Numbers.cs
@@ -10,8 +10,20 @@ namespace _05.Rounding_Numbers
     {
         static void Main(string[] args)
         {
-           
-          
-       }
+            var input = Console.ReadLine().Split(' ').Select(decimal.Parse).ToArray();
+            var rounded = new int[input.Length];
+
+            RoundAndPrintTheNumbers(input, rounded);
+        }
+
+        private static void RoundAndPrintTheNumbers(decimal[] input, int[] rounded)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] > 0) rounded[i] = (int)Math.Floor(input[i] + 0.5m);
+                else rounded[i] = (int)Math.Ceiling(input[i] - 0.5m);
+                Console.WriteLine("{0} => {1}", input[i], rounded[i]);
+            }
+        }
     }
 }
