b2554cc8f93b270c9bc7b3098940e390b8245a93
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-22 22:31:03 +0200

    Solving Condense Array to Number

--- 04. Arrays and Lists/08. Condense Array to Number/08. Condense Array to Number.cs
+++ 04. Arrays and Lists/08. Condense Array to Number/08. Condense Array to Number.cs
@@ -10,7 +10,30 @@ namespace _08.Condense_Array_to_Number
     {
         static void Main(string[] args)
         {
-            
+            var arr1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            arr1 = CondenseTheArray(arr1);
+
+            Console.WriteLine(string.Join(" ", arr1));
+        }
+
+        private static int[] CondenseTheArray(int[] arr1)
+        {
+            while (arr1.Length > 1)
+            {
+                var tempArr = new int[arr1.Length - 1];
+                for (int i = 0; i < tempArr.Length; i++)
+                {
+                    tempArr[i] = arr1[i] + arr1[i + 1];
+                }
+                arr1 = new int[tempArr.Length];
+                for (int i = 0; i < arr1.Length; i++)
+                {
+                    arr1[i] = tempArr[i];
+                }
+            }
+
+            return arr1;
         }
     }
 }
