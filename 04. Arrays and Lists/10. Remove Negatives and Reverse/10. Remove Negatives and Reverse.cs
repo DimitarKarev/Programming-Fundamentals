177dd72326aacbca7611ee39ec349fb79937e29a
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-22 22:59:33 +0200

    Solving Remove Negatives and Reverse

--- 04. Arrays and Lists/10. Remove Negatives and Reverse/10. Remove Negatives and Reverse.cs
+++ 04. Arrays and Lists/10. Remove Negatives and Reverse/10. Remove Negatives and Reverse.cs
@@ -10,7 +10,27 @@ namespace _10.Remove_Negatives_and_Reverse
     {
         static void Main(string[] args)
         {
-           
+            var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<int> numbers = new List<int>();
+            bool empty = true;
+            empty = InsertIntegersInList(array, numbers, empty);
+
+            if (empty) Console.WriteLine("empty");
+            else Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static bool InsertIntegersInList(int[] array, List<int> numbers, bool empty)
+        {
+            foreach (var item in array)
+            {
+                if (item >= 0)
+                {
+                    numbers.Add(item);
+                    empty = false;
+                }
+            }
+            numbers.Reverse();
+            return empty;
         }
     }
 }
