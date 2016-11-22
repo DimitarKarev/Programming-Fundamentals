e5ecf1bbedfd5b281f66c8b544271e0174161a6f
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-22 22:44:55 +0200

    Solving Extract Middle Elements

--- 04. Arrays and Lists/09. Extract Middle Elements/09. Extract Middle Elements.cs
+++ 04. Arrays and Lists/09. Extract Middle Elements/09. Extract Middle Elements.cs
@@ -10,7 +10,18 @@ namespace _09.Extract_Middle_Elements
     {
         static void Main(string[] args)
         {
-            
+            var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var length = array.Length;
+            ExtractMiddleElement(array, length);
+        }
+
+        private static void ExtractMiddleElement(int[] array, int length)
+        {
+            if (length == 1) Console.WriteLine("{ " + array[0] + " }");
+
+            else if (array.Length % 2 == 0) Console.WriteLine("{ " + $"{array[length / 2 - 1]}, {array[length / 2]}" + " }");
+
+            else Console.WriteLine("{ " + $"{array[length / 2 - 1]}, {array[length / 2]}, {array[length / 2 + 1]}" + " }");
         }
     }
 }
