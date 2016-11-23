a2a9dbaf715a4ea8bf1cf49cdeb69dd01ce687a9
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-24 00:22:27 +0200

    Solving Count Numbers

--- 04. Arrays and Lists/16. Count Numbers/16. Count Numbers.cs
+++ 04. Arrays and Lists/16. Count Numbers/16. Count Numbers.cs
@@ -10,7 +10,27 @@ namespace _16.Count_Numbers
     {
         static void Main(string[] args)
         {
-            
+            var nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var counts = new int[1001];
+
+            CountOccurrences(nums, counts);
+            PrintNumbersInAscendingOrder(counts);
+        }
+
+        private static void CountOccurrences(List<int> nums, int[] counts)
+        {
+            foreach (var num in nums)
+            {
+                counts[num]++;
+            }
+        }
+
+        private static void PrintNumbersInAscendingOrder(int[] counts)
+        {
+            for (int i = 0; i < counts.Length; i++)
+            {
+                if (counts[i] > 0) Console.WriteLine($"{i} -> {counts[i]}");
+            }
         }
     }
 }
