8f7be3dfabd212e5b3296818d5afc005927aab6a
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-23 20:02:04 +0200

    Solving Sum Adjacent Equal Numbers

--- 04. Arrays and Lists/12. Sum Adjacent Equal Numbers/12. Sum Adjacent Equal Numbers.cs
+++ 04. Arrays and Lists/12. Sum Adjacent Equal Numbers/12. Sum Adjacent Equal Numbers.cs
@@ -10,7 +10,28 @@ namespace _12.Sum_Adjacent_Equal_Numbers
     {
         static void Main(string[] args)
         {
-           
+            var numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            SumNumbers(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void SumNumbers(List<double> numbers)
+        {
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                while (i < numbers.Count - 1)
+                {
+                    if (numbers[i] == numbers[i + 1])
+                    {
+                        numbers[i] = numbers[i] + numbers[i + 1];
+                        numbers.RemoveAt(i + 1);
+                        if (i > 0) i--;
+                    }
+                    else i++;
+                }
+            }
         }
     }
 }
