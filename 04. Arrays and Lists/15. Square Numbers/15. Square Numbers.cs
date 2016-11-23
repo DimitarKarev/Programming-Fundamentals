bd19fdf4e7ec1dd8c249e01d78dbc508f5daf74e
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-24 00:01:46 +0200

    Solving Square Numbers

--- 04. Arrays and Lists/15. Square Numbers/15. Square Numbers.cs
+++ 04. Arrays and Lists/15. Square Numbers/15. Square Numbers.cs
@@ -10,7 +10,32 @@ namespace _15.Square_Numbers
     {
         static void Main(string[] args)
         {
-            
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var Sqrt = 0.0;
+            var intSqrt = 0;
+
+            ExtractSquareNumbers(numbers, ref Sqrt, ref intSqrt);
+            SortAndPrint(numbers);
+        }
+
+        private static void ExtractSquareNumbers(List<int> numbers, ref double Sqrt, ref int intSqrt)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Sqrt = Math.Sqrt(numbers[i]);
+                intSqrt = (int)Math.Sqrt(numbers[i]);
+                if (Sqrt != intSqrt)
+                {
+                    numbers.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+
+        private static void SortAndPrint(List<int> numbers)
+        {
+            numbers.Sort((a, b) => b.CompareTo(a));
+            Console.WriteLine(string.Join(" ", numbers));
         }
     }
 }
