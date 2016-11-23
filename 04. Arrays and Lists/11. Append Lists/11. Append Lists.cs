7b21cb220abb2ab59188879a75678b75aaf6335f
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-23 09:03:14 +0200

    Solving Append Lists

--- 04. Arrays and Lists/11. Append Lists/11. Append Lists.cs
+++ 04. Arrays and Lists/11. Append Lists/11. Append Lists.cs
@@ -10,7 +10,23 @@ namespace _11.Append_Lists
     {
         static void Main(string[] args)
         {
-            
+            List<string> items = Console.ReadLine().Split('|').ToList();
+            items.Reverse();
+            var result = new List<string>();
+            AppendLists(items, result);
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        private static void AppendLists(List<string> items, List<string> result)
+        {
+            foreach (var item in items)
+            {
+                List<string> nums = item.Split(' ').ToList();
+                foreach (var num in nums)
+                {
+                    if (num != "") result.Add(num);
+                }
+            }
         }
     }
 }
