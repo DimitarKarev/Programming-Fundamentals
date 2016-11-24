ca90a995f469c6bbe8dde6142b394eb4796738e4
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-24 12:43:17 +0200

    Solving Largest Common End

--- 04. Arrays and Lists - Exercises/01. Largest Common End/01. Largest Common End.cs
+++ 04. Arrays and Lists - Exercises/01. Largest Common End/01. Largest Common End.cs
@@ -10,7 +10,38 @@ namespace _01.Largest_Common_End
     {
         static void Main(string[] args)
         {
-            
+            var arr1 = Console.ReadLine().Split().ToArray();
+            var arr2 = Console.ReadLine().Split().ToArray();
+            var shorter = Math.Min(arr1.Length, arr2.Length);
+            var leftLength = 0;
+            var rightLength = 0;
+
+            leftLength = GetLeftLength(arr1, arr2, shorter, leftLength);
+            rightLength = GetRightLength(arr1, arr2, shorter, rightLength);
+
+            Console.WriteLine(Math.Max(leftLength, rightLength));
+        }
+
+        private static int GetLeftLength(string[] arr1, string[] arr2, int shorter, int leftLength)
+        {
+            for (int i = 0; i < shorter; i++)
+            {
+                if (arr1[i] == arr2[i]) leftLength++;
+                else break;
+            }
+
+            return leftLength;
+        }
+
+        private static int GetRightLength(string[] arr1, string[] arr2, int shorter, int rightLength)
+        {
+            for (int i = 0; i < shorter; i++)
+            {
+                if (arr1[arr1.Length - i - 1] == arr2[arr2.Length - i - 1]) rightLength++;
+                else break;
+            }
+
+            return rightLength;
         }
     }
 }
