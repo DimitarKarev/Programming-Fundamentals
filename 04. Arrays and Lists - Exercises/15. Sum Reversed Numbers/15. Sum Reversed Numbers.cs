025f16bb23b13be5a8a2198b1f83769d6fe5d1a6
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-26 16:51:55 +0200

    Solving Sum Reversed Numbers

--- 04. Arrays and Lists - Exercises/15. Sum Reversed Numbers/15. Sum Reversed Numbers.cs
+++ 04. Arrays and Lists - Exercises/15. Sum Reversed Numbers/15. Sum Reversed Numbers.cs
@@ -10,6 +10,36 @@ namespace _15.Sum_Reversed_Numbers
     {
         static void Main(string[] args)
         {
+            var numbers = Console.ReadLine().Split().ToList();
+            List<int> reversed = new List<int>();
+            var sum = 0;
+
+            ReverseNumbers(numbers, reversed);
+            sum = CalcAndPrintSum(reversed, sum);
+        }
+
+        private static void ReverseNumbers(List<string> numbers, List<int> reversed)
+        {
+            foreach (var number in numbers)
+            {
+                var reversedString = string.Empty;
+                number.ToCharArray();
+                for (int i = 0; i < number.Length; i++)
+                {
+                    reversedString += number[number.Length - i - 1];
+                }
+                reversed.Add(int.Parse(reversedString));
+            }
+        }
+
+        private static int CalcAndPrintSum(List<int> reversed, int sum)
+        {
+            foreach (var num in reversed)
+            {
+                sum += num;
+            }
+            Console.WriteLine(sum);
+            return sum;
         }
     }
 }
