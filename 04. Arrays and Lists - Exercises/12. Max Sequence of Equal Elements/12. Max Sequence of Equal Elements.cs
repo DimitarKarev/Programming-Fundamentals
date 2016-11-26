a7a68b1be695bc47d3f05ad85cc9ab45d14f84a3
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-26 16:08:03 +0200

    Solving Max Sequence of Equal Elements

--- 04. Arrays and Lists - Exercises/12. Max Sequence of Equal Elements/12. Max Sequence of Equal Elements.cs
+++ 04. Arrays and Lists - Exercises/12. Max Sequence of Equal Elements/12. Max Sequence of Equal Elements.cs
@@ -10,7 +10,44 @@ namespace _12.Max_Sequence_of_Equal_Elements
     {
         public static void Main()
         {
-            
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> longestSequence = new List<int>();
+            var current = 1;
+            var longest = 1;
+            var number = numbers[0];
+
+            GetLongestSequence(numbers, ref current, ref longest, ref number);
+
+            PrintLongestSequence(longestSequence, longest, number);
+        }
+
+        private static void GetLongestSequence(List<int> numbers, ref int current, ref int longest, ref int number)
+        {
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    current++;
+                    if (current > longest)
+                    {
+                        longest = current;
+                        number = numbers[i];
+                    }
+                }
+                else
+                {
+                    current = 1;
+                }
+            }
+        }
+
+        private static void PrintLongestSequence(List<int> longestSequence, int longest, int number)
+        {
+            for (int i = 0; i < longest; i++)
+            {
+                longestSequence.Add(number);
+            }
+            Console.WriteLine(string.Join(" ", longestSequence));
         }
     }
 }
