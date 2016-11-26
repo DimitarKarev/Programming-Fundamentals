4ee4fa14b9a496d01359b52be05ae325f91e947b
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-26 07:49:37 +0200

    Solving Pairs by Difference

--- 04. Arrays and Lists - Exercises/10. Pairs by Difference/10. Pairs by Difference.cs
+++ 04. Arrays and Lists - Exercises/10. Pairs by Difference/10. Pairs by Difference.cs
@@ -10,7 +10,27 @@ namespace _10.Pairs_by_Difference
     {
         static void Main(string[] args)
         {
-            
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var difference = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            counter = CountPairs(numbers, difference, counter);
+
+            Console.WriteLine(counter);
+        }
+
+        private static int CountPairs(int[] numbers, int difference, int counter)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    if (Math.Abs(numbers[j] - numbers[i]) == difference) counter++;
+
+                }
+            }
+
+            return counter;
         }
     }
 }
