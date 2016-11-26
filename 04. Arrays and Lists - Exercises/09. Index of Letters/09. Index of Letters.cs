ab1f3c848ae1f539607ee3f7e33e0696c0544cd3
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-26 07:34:31 +0200

    Solving Index of Letters

--- 04. Arrays and Lists - Exercises/09. Index of Letters/09. Index of Letters.cs
+++ 04. Arrays and Lists - Exercises/09. Index of Letters/09. Index of Letters.cs
@@ -10,7 +10,26 @@ namespace _09.Index_of_Letters
     {
         static void Main(string[] args)
         {
-            
+            var input = Console.ReadLine().ToCharArray();
+            var alphabet = new char[26];
+            SetAlphabetValues(alphabet);
+            PrintIndex(input, alphabet);
+        }
+        
+        private static void SetAlphabetValues(char[] alphabet)
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                alphabet[i] = (char)('a' + i);
+            }
+        }
+
+        private static void PrintIndex(char[] input, char[] alphabet)
+        {
+            foreach (var item in input)
+            {
+                Console.WriteLine($"{item} -> {Array.IndexOf(alphabet, item)}");
+            }
         }
     }
 }
