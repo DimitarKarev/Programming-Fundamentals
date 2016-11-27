760e99a9eabcd2da99bfff9afadd5c707a2d9f02
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-27 23:18:40 +0200

    Solving Longest Increasing Subsequence

--- 04. Arrays and Lists - Exercises/13. Longest Increasing Subsequence/13. Longest Increasing Subsequence.cs
+++ 04. Arrays and Lists - Exercises/13. Longest Increasing Subsequence/13. Longest Increasing Subsequence.cs
@@ -9,9 +9,51 @@ namespace _13.Longest_Increasing_Subsequence
 {
     class Program
     {
-        static void Main()
+        public static void Main()
         {
-            
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var longestSeq = FindLongestIncreasingSubsequence(numbers);
+
+            Console.WriteLine(string.Join(" ", longestSeq));
+        }
+
+        public static int[] FindLongestIncreasingSubsequence(int[] sequence)
+        {
+            int[] length = new int[sequence.Length];
+            int[] prev = new int[sequence.Length];
+            int maxLength = 0;
+            int lastIndex = -1;
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                length[i] = 1;
+                prev[i] = -1;
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (sequence[j] < sequence[i] && length[j] >= length[i])
+                    {
+                        length[i] = 1 + length[j];
+                        prev[i] = j;
+                    }
+                }
+
+                if (length[i] > maxLength)
+                {
+                    maxLength = length[i];
+                    lastIndex = i;
+                }
+            }
+
+            var longestSeq = new List<int>();
+            for (int i = 0; i < maxLength; i++)
+            {
+                longestSeq.Add(sequence[lastIndex]);
+                lastIndex = prev[lastIndex];
+            }
+
+            longestSeq.Reverse();
+            return longestSeq.ToArray();
         }
     }
 }
