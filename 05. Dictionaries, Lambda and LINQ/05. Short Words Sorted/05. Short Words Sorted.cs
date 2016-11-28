c7574f43396c1345fba393470b47c0cbc986ae4c
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-28 12:11:33 +0200

    Solving Short Words Sorted

--- 05. Dictionaries, Lambda and LINQ/05. Short Words Sorted/05. Short Words Sorted.cs
+++ 05. Dictionaries, Lambda and LINQ/05. Short Words Sorted/05. Short Words Sorted.cs
@@ -10,7 +10,22 @@ namespace _05.Short_Words_Sorted
     {
         static void Main(string[] args)
         {
-           
+            char[] separators = new char[] { '.', ',', ':', ';', '(', ')', '[', ']', '"', '\'', '\\', '/', '!', '?', ' ' };
+
+            List<string> words = GetShortWords(separators);
+
+            Console.WriteLine(string.Join(", ", words));
+
+        }
+
+        private static List<string> GetShortWords(char[] separators)
+        {
+            return Console.ReadLine().ToLower()
+                                     .Split(separators)
+                                     .Where(w => w != "")
+                                     .Where(w => w.Length < 5)
+                                     .OrderBy(w => w).Distinct()
+                                     .ToList();
         }
     }
 }
