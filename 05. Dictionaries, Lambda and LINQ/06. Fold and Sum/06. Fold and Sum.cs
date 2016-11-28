094cb8548eec6eadd8bccbbdc072113b64d02454
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-28 12:24:27 +0200

    Solving Fold and Sum

--- 05. Dictionaries, Lambda and LINQ/06. Fold and Sum/06. Fold and Sum.cs
+++ 05. Dictionaries, Lambda and LINQ/06. Fold and Sum/06. Fold and Sum.cs
@@ -10,7 +10,49 @@ namespace _06.Fold_and_Sum
     {
         static void Main(string[] args)
         {
-            
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int k = numbers.Length / 4;
+
+            int[] row1Left = GetLeft(numbers, k);
+            int[] row1Right = GetRight(numbers, k);
+            int[] row1 = GetRow1(row1Left, row1Right);
+            int[] row2 = GetRow2(numbers, k);
+            SumAndPrint(row1, row2);
+
+        }
+
+        private static int[] GetLeft(int[] numbers, int k)
+        {
+            return numbers.Take(k)
+                          .Reverse()
+                          .ToArray();
+        }
+
+        private static int[] GetRight(int[] numbers, int k)
+        {
+            return numbers.Reverse()
+                          .Take(k)
+                          .ToArray();
+        }
+
+        private static int[] GetRow1(int[] row1Left, int[] row1Right)
+        {
+            return row1Left.Concat(row1Right)
+                           .ToArray();
+        }
+
+        private static int[] GetRow2(int[] arr, int k)
+        {
+            return arr.Skip(k)
+                      .Take(2 * k)
+                      .ToArray();
+        }
+
+        private static void SumAndPrint(int[] row1, int[] row2)
+        {
+            var sum = row1.Select((x, index) => x + row2[index]);
+
+            Console.WriteLine(string.Join(" ", sum));
         }
     }
 }
