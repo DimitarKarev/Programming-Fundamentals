244879cc62d818123984daa78aa1297c094b7acf
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-28 11:56:51 +0200

    Solving Min, Max, Sum, Average

--- 05. Dictionaries, Lambda and LINQ/03. Min, Max, Sum, Average/03. Min, Max, Sum, Average.cs
+++ 05. Dictionaries, Lambda and LINQ/03. Min, Max, Sum, Average/03. Min, Max, Sum, Average.cs
@@ -10,7 +10,27 @@ namespace _03.Min__Max__Sum__Average
     {
         static void Main(string[] args)
         {
-            
+            var n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            EnterNumberValues(n, numbers);
+
+            Print(numbers);
+        }
+
+        private static void EnterNumberValues(int n, int[] numbers)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        private static void Print(int[] numbers)
+        {
+            Console.WriteLine("Sum = {0}", numbers.Sum());
+            Console.WriteLine("Min = {0}", numbers.Min());
+            Console.WriteLine("Max = {0}", numbers.Max());
+            Console.WriteLine("Average = {0}", numbers.Average());
         }
     }
 }
