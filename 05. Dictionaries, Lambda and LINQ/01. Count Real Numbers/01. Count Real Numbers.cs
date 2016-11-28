18610068040b9c008115b1ed3dff9c0b61d9205d
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-28 11:36:29 +0200

    Solving Count Real Numbers

--- 05. Dictionaries, Lambda and LINQ/01. Count Real Numbers/01. Count Real Numbers.cs
+++ 05. Dictionaries, Lambda and LINQ/01. Count Real Numbers/01. Count Real Numbers.cs
@@ -10,7 +10,34 @@ namespace _01.Count_Real_Numbers
     {
         static void Main(string[] args)
         {
-            
+            var numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            CountOccurrences(numbers, counts);
+            PrintNumbers(counts);
+        }
+
+        private static void CountOccurrences(double[] numbers, SortedDictionary<double, int> counts)
+        {
+            foreach (var number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts[number] = 1;
+                }
+            }
+        }
+
+        private static void PrintNumbers(SortedDictionary<double, int> counts)
+        {
+            foreach (var pair in counts)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
         }
     }
 }
