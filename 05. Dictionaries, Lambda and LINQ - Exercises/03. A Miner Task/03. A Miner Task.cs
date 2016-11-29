03a6fbacca7a741870fbe1f0082b7677d29da4d2
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-29 11:08:40 +0200

    Solving A Miner Task

--- 05. Dictionaries, Lambda and LINQ - Exercises/03. A Miner Task/03. A Miner Task.cs
+++ 05. Dictionaries, Lambda and LINQ - Exercises/03. A Miner Task/03. A Miner Task.cs
@@ -10,7 +10,43 @@ namespace _03.A_Miner_Task
     {
         static void Main(string[] args)
         {
-            
+            Dictionary<string, int> element = new Dictionary<string, int>();
+            string input = Console.ReadLine();
+            if (input == "stop")
+            {
+            return;
+            }
+            int quantity = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                GetElementAndQuantity(element, input, quantity);
+
+                input = Console.ReadLine();
+
+                if (input.Equals("stop"))
+                {
+                    break;
+                }
+
+                quantity = int.Parse(Console.ReadLine());
+            }
+            foreach (var item in element)
+            {
+                Console.WriteLine(item.Key + " -> " + item.Value);
+            }
+        }
+
+        private static void GetElementAndQuantity(Dictionary<string, int> element, string input, int quantity)
+        {
+            if (element.ContainsKey(input))
+            {
+                element[input] += quantity;
+            }
+            else
+            {
+                element.Add(input, quantity);
+            }
         }
     }
 }
