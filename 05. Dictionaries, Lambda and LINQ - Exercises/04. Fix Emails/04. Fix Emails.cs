5527647a665a3c50474c3e28d26207d10d2f36ce
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-11-29 23:33:59 +0200

    Solving Fix Emails

--- 05. Dictionaries, Lambda and LINQ - Exercises/04. Fix Emails/04. Fix Emails.cs
+++ 05. Dictionaries, Lambda and LINQ - Exercises/04. Fix Emails/04. Fix Emails.cs
@@ -10,7 +10,44 @@ namespace _04.Fix_Emails
     {
         static void Main(string[] args)
         {
-            
+            var name = Console.ReadLine();
+            if (name.Equals("stop"))
+            {
+                return;
+            }
+            var email = Console.ReadLine();
+            Dictionary<string, string> emails = new Dictionary<string, string>();
+
+            while (!name.Equals("stop"))
+            {
+                string[] emailParts = email.Split('.').ToArray();
+
+                GetEmails(name, email, emails, emailParts);
+
+                name = Console.ReadLine();
+                if (name.Equals("stop"))
+                {
+                    break;
+                }
+                email = Console.ReadLine();
+            }
+            PrintEmails(emails);
+        }
+
+        private static void GetEmails(string name, string email, Dictionary<string, string> emails, string[] emailParts)
+        {
+            if (!emailParts.Contains("us") && !emailParts.Contains("uk"))
+            {
+                emails[name] = email;
+            }
+        }
+
+        private static void PrintEmails(Dictionary<string, string> emails)
+        {
+            foreach (var pair in emails)
+            {
+                Console.WriteLine("{0} -> {1}", pair.Key, pair.Value);
+            }
         }
     }
 }
