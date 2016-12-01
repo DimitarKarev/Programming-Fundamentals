dd5b4a229008cbdc228cd35bfe3696873743e848
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-12-01 11:37:04 +0200

    Solving Distance Between Points

--- 06. Objects and Classes/04. Distance between Points/04. Distance between Points.cs
+++ 06. Objects and Classes/04. Distance between Points/04. Distance between Points.cs
@@ -9,9 +9,35 @@ namespace _04.Distance_between_Points
     class Program
     {
         static void Main(string[] args)
-        {
-           
-            
+        {   
+            Point p1 = ReadPoint();
+            Point p2 = ReadPoint();
+
+            double distance = CalculateDistance(p1, p2);
+
+            Console.WriteLine(distance);
+
         }
+        static Point ReadPoint()
+        {
+            int[] pointInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Point point = new Point();
+            point.X = pointInfo[0];
+            point.Y = pointInfo[1];
+            return point;
+        }
+
+        private static double CalculateDistance(Point p1, Point p2)
+        {
+            int x = Math.Abs(p1.X - p2.X);
+            int y = Math.Abs(p1.Y - p2.Y);
+            double distance = Math.Sqrt(x * x + y * y);
+            return distance;
+        }
+    }
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
     }
 }
