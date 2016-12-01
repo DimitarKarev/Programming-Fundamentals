aa7d1abe0f41fb1d591b54530ad70798183b76e9
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-12-01 10:37:00 +0200

    Solving Big Factorial

--- 06. Objects and Classes/03. Big Factorial/03. Big Factorial.cs
+++ 06. Objects and Classes/03. Big Factorial/03. Big Factorial.cs
@@ -11,7 +11,10 @@ namespace _03.Big_Factorial
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+            for (int i = 2; i <= n; i++) factorial *= i;
+            Console.WriteLine(factorial);
         }
     }
 }
