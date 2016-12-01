f536e94285d50b18ac77784e77d08dad2252c683
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-12-01 10:19:14 +0200

    Solving Randomize Words

--- 06. Objects and Classes/02. Randomize Words/02. Randomize Words.cs
+++ 06. Objects and Classes/02. Randomize Words/02. Randomize Words.cs
@@ -10,7 +10,23 @@ namespace _02.Randomize_Words
     {
         static void Main(string[] args)
         {
-            
+            string[] words = Console.ReadLine().Split().ToArray();
+            Random rnd = new Random();
+
+            RandomiseWords(words, rnd);
+
+            Console.WriteLine(string.Join("\r\n", words));
+        }
+
+        private static void RandomiseWords(string[] words, Random rnd)
+        {
+            for (int pos1 = 0; pos1 < words.Length; pos1++)
+            {
+                int pos2 = rnd.Next(words.Length);
+                string temp = words[pos1];
+                words[pos1] = words[pos2];
+                words[pos2] = temp;
+            }
         }
     }
 }
