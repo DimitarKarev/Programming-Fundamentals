eb07f26abe6e0cc46979b45ea5bb07a16703706a
Dimitar Karev <Dimitar Karev>
2016-12-02 22:59:44 +0200

    Solving Rectangle Position

--- 06. Objects and Classes/06. Rectangle Position/06. Rectangle Position.cs
+++ 06. Objects and Classes/06. Rectangle Position/06. Rectangle Position.cs
@@ -10,7 +10,68 @@ namespace _06.Rectangle_Position
     {
         static void Main(string[] args)
         {
-            
+            Rectangle rect1 = ReadRectangle();
+            Rectangle rect2 = ReadRectangle();
+
+            PrintOutput(rect1, rect2);
+        }
+
+        static Rectangle ReadRectangle()
+        {
+            int[] rectInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Rectangle rect = new Rectangle();
+            rect.Left = rectInfo[0];
+            rect.Top = rectInfo[1];
+            rect.Width = rectInfo[2];
+            rect.Height = rectInfo[3];
+            return rect;
+        }
+
+        static void PrintOutput(Rectangle rect1, Rectangle rect2)
+        {
+            if (rect1.IsInside(rect2))
+            {
+                Console.WriteLine("Inside");
+            }
+            else
+            {
+                Console.WriteLine("Not Inside");
+            }
+        }
+
+    }
+    class Rectangle
+    {
+        public int Left { get; set; }
+        public int Top { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public int Bottom
+        {
+            get
+            {
+                return Top + Height;
+            }
+        }
+
+        public int Right
+        {
+            get
+            {
+                return Left + Width;
+            }
+        }
+
+        public int CalcArea()
+        {
+            return Width * Height;
+        }
+
+        public bool IsInside(Rectangle rect)
+        {
+            return (rect.Left <= Left) && (rect.Right >= Right) &&
+               (rect.Top <= Top) && (rect.Bottom >= Bottom);
         }
 
     }
