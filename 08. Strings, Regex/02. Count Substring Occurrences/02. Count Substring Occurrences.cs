161d64dd83630bc8bbd835d23f14a6610d6012a4
Dimitar Karev <Dimitar Karev>
2016-12-03 13:22:37 +0200

    Solving Count Substring Occurrences

--- 08. Strings, Regex/02. Count Substring Occurrences/02. Count Substring Occurrences.cs
+++ 08. Strings, Regex/02. Count Substring Occurrences/02. Count Substring Occurrences.cs
@@ -10,7 +10,25 @@ namespace _02.Count_Substring_Occurrences
     {
         static void Main(string[] args)
         {
-            
+            string input = Console.ReadLine().ToLower();
+            string pattern = Console.ReadLine().ToLower();
+            int counter = 0;
+
+            counter = CountOccurrences(input, pattern, counter);
+
+            Console.WriteLine(counter);
+        }
+
+        private static int CountOccurrences(string input, string pattern, int counter)
+        {
+            int index = input.IndexOf(pattern);
+            while (index != -1)
+            {
+                counter++;
+                index = input.IndexOf(pattern, index + 1);
+            }
+
+            return counter;
         }
     }
 }
