854bb7cc82ab295bfb861ce00c65718d393460dc
Dimitar Karev <Dimitar Karev>
2016-12-03 13:32:30 +0200

    Solving Text Filter

--- 08. Strings, Regex/03. Text Filter/03. Text Filter.cs
+++ 08. Strings, Regex/03. Text Filter/03. Text Filter.cs
@@ -10,7 +10,26 @@ namespace _03.Text_Filter
     {
         static void Main(string[] args)
         {
-            
+            char[] splitSymbols = new char[] { ' ', ',' };
+            string[] bannedWords = Console.ReadLine().Split(splitSymbols, StringSplitOptions.RemoveEmptyEntries);
+            var text = Console.ReadLine();
+
+            text = FilterText(bannedWords, text);
+
+            Console.WriteLine(text);
+        }
+
+        private static string FilterText(string[] bannedWords, string text)
+        {
+            foreach (var banWord in bannedWords)
+            {
+                if (text.Contains(banWord))
+                {
+                    text = text.Replace(banWord, new string('*', banWord.Length));
+                }
+            }
+
+            return text;
         }
     }
 }
