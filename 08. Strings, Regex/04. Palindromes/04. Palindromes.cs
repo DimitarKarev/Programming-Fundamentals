10d4fa0d1fe5c19ff98e1d9bbc62fc77463e31bb
Dimitar Karev <Dimitar Karev>
2016-12-03 13:39:39 +0200

    Solving Palindromes

--- 08. Strings, Regex/04. Palindromes/04. Palindromes.cs
+++ 08. Strings, Regex/04. Palindromes/04. Palindromes.cs
@@ -10,7 +10,38 @@ namespace _04.Palindromes
     {
         static void Main(string[] args)
         {
-            
+            char[] splitSymbols = { ',', ' ', '?', '!', '.' };
+            string[] words = Console.ReadLine().Split(splitSymbols, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            List<string> palindromes = new List<string>();
+
+            GetPalindromes(words, palindromes);
+
+            palindromes = palindromes.Distinct().OrderBy(x => x).ToList();
+            Console.WriteLine(string.Join(", ", palindromes));
+        }
+
+        private static void GetPalindromes(string[] words, List<string> palindromes)
+        {
+            for (int word = 0; word < words.Length; word++)
+            {
+                string myString = words[word];
+                bool palindrome = getStatus(myString);
+                if (palindrome)
+                {
+                    palindromes.Add(myString);
+                }
+            }
+        }
+
+        public static bool getStatus(string myString)
+        {
+            string first = myString.Substring(0, myString.Length / 2);
+            char[] arr = myString.ToCharArray();
+            Array.Reverse(arr);
+            string temp = new string(arr);
+            string second = temp.Substring(0, temp.Length / 2);
+
+            return first.Equals(second);
         }
     }
- }
+}
