816e31532e6b2fc9f8b3c8299faebe39b80757b1
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-12-09 19:09:22 +0200

    Solving Circles Intersection

--- 06. Objects and Classes - Exercises/03. Circles Intersection/03. Circles Intersection.cs
+++ 06. Objects and Classes - Exercises/03. Circles Intersection/03. Circles Intersection.cs
@@ -10,6 +10,84 @@ namespace _03.Circles_Intersection
     {
         static void Main(string[] args)
         {
+            int[] firstCircleParams = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondCircleParams = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Circle firstCircle = GetFirstCircle(firstCircleParams);
+            Circle secondCircle = GetSecondCircle(secondCircleParams);
+
+            PrintResult(firstCircle, secondCircle);
+
+        }
+
+        private static Circle GetFirstCircle(int[] firstCircleParams)
+        {
+            Point firstPoint = new Point
+            {
+                X = firstCircleParams[0],
+                Y = firstCircleParams[1]
+            };
+
+            Circle firstCircle = new Circle
+            {
+                Radius = firstCircleParams[2],
+                Center = firstPoint
+            };
+            return firstCircle;
+        }
+
+        private static Circle GetSecondCircle(int[] secondCircleParams)
+        {
+            Point secondPoint = new Point
+            {
+                X = secondCircleParams[0],
+                Y = secondCircleParams[1]
+            };
+
+            Circle secondCircle = new Circle
+            {
+                Radius = secondCircleParams[2],
+                Center = secondPoint
+            };
+            return secondCircle;
+        }
+
+        private static void PrintResult(Circle firstCircle, Circle secondCircle)
+        {
+            if (Circle.Intersect(firstCircle, secondCircle))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+    
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class Circle
+    {
+        public int Radius { get; set; }
+        public Point Center { get; set; }
+
+        public static bool Intersect (Circle firstCircle, Circle secondCircle)
+        {
+            int deltaX = Math.Abs(firstCircle.Center.X - secondCircle.Center.X);
+            int deltaY = Math.Abs(firstCircle.Center.Y - secondCircle.Center.Y);
+            double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+            int radiusSum = firstCircle.Radius + secondCircle.Radius;
+
+            if (distance <= radiusSum)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
