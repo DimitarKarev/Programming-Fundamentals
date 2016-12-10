6416b8df26a0fb7a5db5b7915c23a995c35ffc2f
DimitarKarev <Dimitarkarevdslav@gmail.com>
2016-12-10 10:38:35 +0200

    Solving Average Grades

--- 06. Objects and Classes - Exercises/04. Average Grades/04. Average Grades.cs
+++ 06. Objects and Classes - Exercises/04. Average Grades/04. Average Grades.cs
@@ -5,7 +5,59 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace _04.Average_Grades
-{
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+
+            for (int name = 0; name < n; name++)
+            {
+                Student currentStudent = new Student();
+                List<string> studentData = Console.ReadLine().Split().ToList();
+
+                GetStudentData(currentStudent, studentData);
+
+                students.Add(currentStudent);
+            }
+
+            List<Student> notableStudents = students.OrderBy(student => student.name).ThenByDescending(student => student.averageGrade).ToList();
+            PrintNotableStudents(notableStudents);
+        }
+
+        private static void GetStudentData(Student currentStudent, List<string> studentData)
+        {
+            currentStudent.name = studentData[0];
+            List<double> currentStudentGrades = new List<double>();
+
+            GetGrades(studentData, currentStudentGrades);
+
+            currentStudent.grades = currentStudentGrades;
+        }
+
+        private static void GetGrades(List<string> studentData, List<double> currentStudentGrades)
+        {
+            for (int grade = 1; grade < studentData.Count; grade++)
+            {
+                double studentGrade = double.Parse(studentData[grade]);
+                currentStudentGrades.Add(studentGrade);
+            }
+        }
+
+        private static void PrintNotableStudents(List<Student> notableStudents)
+        {
+            foreach (var student in notableStudents)
+            {
+                if (student.averageGrade >= 5)
+                {
+                    Console.WriteLine("{0:f2} -> {1:f2}", student.name, student.averageGrade);
+                }
+            }
+        }
+    }
+
     class Student
     {
         public string name { get; set; }
@@ -16,13 +68,6 @@ namespace _04.Average_Grades
             {
                 return grades.Average();
             }
-        }
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
         }
     }
 }
